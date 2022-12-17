b2327ad060f13f1d7c766ad6d0293217d136a86a
Ivanna Honcharuk <Ivannatsyganjuk@gmail.com>
2022-12-17 23:36:27 +0100

    add global style, box, container, fonts, theme in styled components

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
-import 'modern-normalize';
-import './index.css';
+import { theme } from 'theme';
+import { ThemeProvider } from 'styled-components';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/">
-      <App />
+      <ThemeProvider theme = {theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
