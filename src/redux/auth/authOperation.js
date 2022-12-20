d87054554b594b883c7ec1736fdf740c09cc0d00
Denys Kuz <dkuz@gomage.com>
2022-12-20 22:03:34 +0200

    SLIMMOM: -------

--- src/redux/auth/authOperation.js
+++ src/redux/auth/authOperation.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-const BASE_URL = process.env.BACKEND_URL;
+const BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 axios.defaults.baseURL = BASE_URL;
 
