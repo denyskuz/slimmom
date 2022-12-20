55fd9d3686b7699e6185beb06f6d06b6d5e975a9
Denys Kuz <dkuz@gomage.com>
2022-12-20 22:31:44 +0200

    SLIMMOM: -------

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,5 +1,6 @@
 import { DailyCaloriesForm } from 'components/DailyCaloriesForm/DailyCaloriesForm';
 import { Wrapper } from './MainPage.styled';
+
 export default function MainPage() {
   return (
     <main>
