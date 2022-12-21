66b37b4b469bfc2f9163a36636e0c4a2c482832b
ikarkam76 <ikarkam76@gmail.com>
2022-12-21 13:04:20 +0200

    add BG to register page

--- src/pages/registration/registrationPage.js
+++ src/pages/registration/registrationPage.js
@@ -1,6 +1,6 @@
 import RegisterForm from 'components/RegistrationForm/registrationForm';
 import { Helmet } from 'react-helmet';
-import { Title } from './registrationPage.styled';
+import { Title, Wrapper } from './registrationPage.styled';
 import Container from 'components/Container';
 
 export default function RegistrationPage() {
@@ -9,10 +9,12 @@ export default function RegistrationPage() {
       <Helmet>
         <title>Register</title>
       </Helmet>
-      <Container>
-        <Title>Register</Title>
-        <RegisterForm />
-      </Container>
+      <Wrapper>
+        <Container>
+          <Title>Register</Title>
+          <RegisterForm />
+        </Container>
+      </Wrapper>
     </>
   );
 }
