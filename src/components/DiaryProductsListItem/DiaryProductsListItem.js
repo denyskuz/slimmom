d085f8bacc44b35e8a320ad7ace6e515497a5c1a
Yevhen Bashchak <evgeny221@gmail.com>
2022-12-21 12:28:58 +0100

    diary

---
+++ src/components/DiaryProductsListItem/DiaryProductsListItem.js
@@ -0,0 +1,110 @@
+export const data = [
+  {
+    _id: {
+      $oid: '5d51694802b2373622ff554d',
+    },
+    categories: ['зерновые'],
+    weight: 100,
+    title: {
+      ru: 'Горох маш Ярмарка Платинум',
+      ua: 'Горох маш Ярмарка Платинум',
+    },
+    calories: 312,
+    groupBloodNotAllowed: [null, true, false, false, false],
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: '5d51694802b2373622ff555c',
+    },
+    categories: ['зерновые'],
+    weight: 100,
+    title: {
+      ru: 'Гречневая крупа (ядрица) зелёная',
+      ua: 'Гречана крупа (ядриця) зелена',
+    },
+    calories: 296,
+    groupBloodNotAllowed: [null, true, false, true, true],
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: '5d51694802b2373622ff553a',
+    },
+    categories: ['яйца'],
+    weight: 100,
+    title: {
+      ru: 'Яйцо куриное (вареное всмятку)',
+      ua: 'Яйце куряче (варене всмятку)',
+    },
+    calories: 159,
+    groupBloodNotAllowed: [null, true, false, false, false],
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: '5d51694802b2373622ff5530',
+    },
+    categories: ['яйца'],
+    weight: 100,
+    title: {
+      ru: 'Омлет с сыром',
+      ua: 'Омлет із сиром',
+    },
+    calories: 342,
+    groupBloodNotAllowed: [null, true, false, false, false],
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: '5d51694802b2373622ff5539',
+    },
+    categories: ['яйца'],
+    weight: 100,
+    title: {
+      ru: 'Яйцо куриное (вареное вкрутую)',
+      ua: 'Яйце куряче (варене круто)',
+    },
+    calories: 160,
+    groupBloodNotAllowed: [null, true, false, false, false],
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: '5d51694802b2373622ff552c',
+    },
+    categories: ['яйца'],
+    weight: 100,
+    title: {
+      ru: 'Меланж яичный',
+      ua: 'Меланж яєчний',
+    },
+    calories: 157,
+    groupBloodNotAllowed: [null, true, false, false, false],
+    __v: 0,
+  },
+];
+
+export default function DiaryProductsListItem({ data }) {
+  const handleDelete = ({ $oid }) => {
+    console.log($oid);
+  };
+  return data.map((e, i, ar) => {
+    // console.log(e.id);
+    return (
+      <tr key={e._id.$oid}>
+        <td>{e.title.ua}</td>
+        <td>{e.weight} g</td>
+        <td>{e.calories}kcal</td>
+        <td>
+          <button
+            type="button"
+            onClick={() => {
+              handleDelete(e._id);
+            }}
+          ></button>
+        </td>
+      </tr>
+    );
+  });
+}
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,14 @@
+import DiaryProductsListItem from 'components/DiaryProductsListItem/DiaryProductsListItem';
+import { data } from 'components/DiaryProductsListItem/DiaryProductsListItem';
 export default function LoginPage() {
   return (
     <main>
-      <h1>LoginPage</h1>
+      <h1>test</h1>
+      <table>
+        <tbody>
+          <DiaryProductsListItem data={data} />
+        </tbody>
+      </table>
     </main>
   );
 }
