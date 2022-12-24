882247ace38b2e4c37b88b1121b0f23b32f0094b
Yevhen Bashchak <evgeny221@gmail.com>
2022-12-24 17:43:42 +0100

    inWork-of-autocomplete

--- src/components/DiaryAddProductForm/AddForm/AddForm.js
+++ src/components/DiaryAddProductForm/AddForm/AddForm.js
@@ -1,17 +1,18 @@
-import { AddProductBtn } from 'components/Button/Button';
 import { HiPlus } from 'react-icons/hi';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
+import { AddProductBtn } from 'components/Button/Button';
 import { Form, ProductInput, GramsInput } from './AddForm.styled';
 import { useDispatch } from 'react-redux';
 import { addDiaryProduct } from 'redux/services/operations';
-
+import { data } from 'globalstore/dataTemp';
+import { Autocomplete, TextField } from '@mui/material';
 const AddForm = () => {
   const dispatch = useDispatch();
   const date = new Date().toISOString();
   const formik = useFormik({
-    initialValues: { product: '', weight: '' },
+    initialValues: { product: '', weight: '', auto: '' },
     validationSchema: Yup.object().shape({
       product: Yup.string().required(),
       weight: Yup.number().min(2).required(),
@@ -19,13 +20,11 @@ const AddForm = () => {
     onSubmit: (values, { resetForm }) => {
       const data = { ...values, date };
       console.log(data);
-      dispatch(addDiaryProduct(data));
-      resetForm();
-    },
-    onChange: (values, _) => {
-      console.log(values);
+      dispatch(addDiaryProduct(date));
+      // resetForm();
     },
   });
+  const nameProd = data.map(e => e.title.ua);
 
   return (
     <Form onSubmit={formik.handleSubmit}>
@@ -41,6 +40,29 @@ const AddForm = () => {
         value={formik.values.product}
         error={formik.touched.product && formik.errors.product}
         helperText={formik.touched.product && formik.errors.product}
+      />
+      <Autocomplete
+        onChange={(e, value) => console.log(value)}
+        value={formik.values.auto}
+        id="auto"
+        name="auto"
+        freeSolo
+        disableClearable
+        options={nameProd.map(option => option)}
+        renderInput={params => (
+          <TextField
+            {...params}
+            label="Search input"
+            InputProps={{
+              ...params.InputProps,
+              type: 'search',
+              // onChange: formik.handleChange,
+              // value: formik.values.auto,
+              // id: 'auto',
+              // name: 'auto',
+            }}
+          />
+        )}
       />
       <GramsInput
         id="weight"
