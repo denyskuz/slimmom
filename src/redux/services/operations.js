aa98657742d72486db9f56f70da2983ce240e5ec
Denys Kuz <dkuz@gomage.com>
2022-12-24 11:14:37 +0200

    SLIMMOM: ----

--- src/redux/services/operations.js
+++ src/redux/services/operations.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { createAsyncThunk, createAction } from '@reduxjs/toolkit';
 import toast from 'react-hot-toast';
 
-const BASE_URL = 'http://localhost:3000/'; //process.env.REACT_APP_BACKEND_URL;
+const BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 axios.defaults.baseURL = BASE_URL;
 
