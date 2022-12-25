7e4898e0ababd1dcbcdf86f39152314576c57338
SvitlanaVelychko <svel2608@gmail.com>
2022-12-25 21:54:34 +0200

    Merge main into fix-background

--- src/pages/MainPage/MainPage.styled.js
+++ src/pages/MainPage/MainPage.styled.js
@@ -5,10 +5,8 @@ import backgroundDesktop from '../../images/background/background-desktop.png';
 export const Wrapper = styled.div`
   height: calc(100vh - 82px);
 
-
   @media (min-width: 767px) {
     background: url(${backgroundTablet}) no-repeat bottom 0 right 0 / 100% 60%;
-
   }
   @media (min-width: 1280px) {
     background: url(${backgroundDesktop}) no-repeat bottom 0 right 0 / 60% 100%;
