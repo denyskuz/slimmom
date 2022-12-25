918c69fce1141a2a8c3950e7b7562e13f470fbb8
ikarkam76 <ikarkam76@gmail.com>
2022-12-25 21:56:31 +0200

    again

--- src/components/RegistrationForm/registrationForm.js
+++ src/components/RegistrationForm/registrationForm.js
@@ -18,7 +18,7 @@ const RegisterForm = () => {
   const dispatch = useDispatch();
   const [showPassword, setShowPassword] = useState(false);
   const handleClickShowPassword = () => setShowPassword(show => !show);
-  const handleMouseDownPassword = (event) => {
+  const handleMouseDownPassword = event => {
     event.preventDefault();
   };
 
@@ -64,16 +64,18 @@ const RegisterForm = () => {
         variant="standard"
         type={showPassword ? 'text' : 'password'}
         InputProps={{
-          endAdornment: <InputAdornment position="end">
-            <IconButton
-              aria-label="toggle password visibility"
-              onClick={handleClickShowPassword}
-              onMouseDown={handleMouseDownPassword}
-              edge="end"
-            >
-              {showPassword ? <VisibilityOff /> : <Visibility />}
-            </IconButton>
-          </InputAdornment>
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          ),
         }}
         onChange={formik.handleChange}
         value={formik.values.password}
