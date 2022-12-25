f817f45785843d3b1a0c1a9bdc5e2469b0078eb8
ikarkam76 <ikarkam76@gmail.com>
2022-12-25 20:53:30 +0200

    added menu to product list

--- src/components/DailyCalorieIntake/FoodListNotEat.js
+++ src/components/DailyCalorieIntake/FoodListNotEat.js
@@ -3,19 +3,31 @@ import { number, arrayOf, object, string } from 'prop-types';
 import Box from '@mui/material/Box';
 import ListItemButton from '@mui/material/ListItemButton';
 import List from '@mui/material/List';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown';
 import { ListText, ProductListText } from './FoodListNotEat.styled';
 
 export const CustomizedList = ({ number, categorie, list }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
   const [open, setOpen] = React.useState(false);
+    const isOpen = Boolean(anchorEl);
+    const handleClick = event => {
+      setAnchorEl(event.currentTarget);
+      setOpen(!open);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+  };
+  
   return (
     <Box>
       <ListItemButton
         alignItems="flex-start"
-        onClick={() => setOpen(!open)}
+        onClick={handleClick}
         sx={{
-          px: 1,
-          pt: 1,
+          px: 0,
+          pt: 0,
           pb: open ? 0 : 1,
           '&:hover, &:focus': { '& svg': { opacity: open ? 1 : 0 } },
         }}
@@ -49,12 +61,24 @@ export const CustomizedList = ({ number, categorie, list }) => {
         }}
         subheader={<li />}
       >
-        {open &&
-          list.map(item => (
-            <li key={item.title.ru}>
-              <ProductListText primary={'-  ' + item.title.ru} />
-            </li>
-          ))}
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={isOpen}
+          onClose={handleClose}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+          }}
+        >
+          {open &&
+            list.map(item => (
+              <li key={item.title.ru}>
+                <MenuItem onClick={handleClose}>
+                  <ProductListText primary={'-  ' + item.title.ru} />
+                </MenuItem>
+              </li>
+            ))}
+        </Menu>
       </List>
     </Box>
   );
