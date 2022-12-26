c91849f29078098a1cc711ac60bb3ae495dfd35f
ikarkam76 <ikarkam76@gmail.com>
2022-12-26 15:22:57 +0200

    added styles and categories on product list

--- src/redux/services/selectors.js
+++ src/redux/services/selectors.js
@@ -19,3 +19,5 @@ export const selectBadProducts = state => state.products.bad;
 export const selectCalories = state => state.products.calories;
 export const selectLoadStatus = state => state.products.loading;
 export const selectLoadError = state => state.products.error;
+export const selectBadCategories = state => state.products.categories;
+
