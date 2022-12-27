1112a19466ad3c7ec932d29267c7d0896d7494ee
natalia-stebel <93938115+natalia-stebel@users.noreply.github.com>
2022-12-27 12:54:09 +0200

    fix

--- src/components/Header/AuthNav/AuthNav.js
+++ src/components/Header/AuthNav/AuthNav.js
@@ -1,63 +1,45 @@
-import { Link } from './AuthNav.styled';
+import { Link, LangSwitcher, LangList, LangButton } from './AuthNav.styled';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-
 import i18n from './../../../translations/i18n';
 import '/node_modules/flag-icons/css/flag-icons.min.css';
-import { Dropdown } from 'semantic-ui-react';
 
 const AuthNav = () => {
   const { t } = useTranslation();
+  const [droped, setDroped] = useState('');
   const [language, setLanguage] = useState('en');
 
-  const handleOnclick = e => {
-    e.preventDefault();
-    setLanguage(e.target.value);
-    i18n.changeLanguage(e.target.value);
-    localStorage.setLanguage('locale', language);
+  const hadleChangeLang = language => {
+    i18n.changeLanguage(language);
+    setLanguage(language);
   };
-  const langdOptions = [
-    {
-      type: 'button',
-      key: 'EN',
-      text: 'EN',
-      value: 'EN',
-      image: { avatar: true, src: '/images/avatar/small/jenny.jpg' },
-    },
-    {
-      type: 'button',
-      key: 'UA',
-      text: 'UA',
-      value: 'UA',
-      image: { avatar: true, src: '/images/avatar/small/elliot.jpg' },
-    },
-  ];
+
+  // const handleOnclick = e => {
+  //   // e.preventDefault();
+  //   setLanguage(e.target.value);
+  //   i18n.changeLanguage(e.target.value);
+  // };
+
   return (
     <>
-      <Dropdown
-        inline
-        options={langdOptions}
-        defaultValue={langdOptions[0].value}
-        onClick={handleOnclick}
-      />
-      {/* <ButtonContainer>
-        <LangButton
-          value="en"
-          onClick={handleOnclick}
-          className="fi fi-gb"
-        ></LangButton>
-        <LangButton
-          value="uk"
-          onClick={handleOnclick}
-          className="fi fi-ua"
-        ></LangButton>
-      </ButtonContainer> */}
-      <Link to="/login" lang={language}>
-        {t('Sign_in')}
-      </Link>
-      <Link to="/signup" lang={language}>
-        {t('Registration')}
-      </Link>
+      <LangSwitcher onClick={() => setDroped(!droped)}>
+        <LangList className={droped ? 'droped' : ''}>
+          {i18n.languages.map(lang => (
+            <LangButton
+              className={lang === i18n.language ? 'active' : ''}
+              type="button"
+              onClick={() => hadleChangeLang(language)}
+              key={language}
+            >
+              {lang}
+            </LangButton>
+          ))}
+        </LangList>
+      </LangSwitcher>
+
+      <Link to="/login">{t('Sign_in')}</Link>
+      <Link to="/signup">{t('Registration')}</Link>
+      {/* {console.log(i18n.languages)} */}
     </>
   );
 };
--- src/components/Header/AuthNav/AuthNav.styled.js
+++ src/components/Header/AuthNav/AuthNav.styled.js
@@ -4,54 +4,134 @@ import styled from 'styled-components';
 export const Link = styled(NavLink)`
   padding: 10px;
   font-family: GothamPro-Bold;
-  font-size: ${prop => prop.theme.fontSizes.xs};
-  font-weight: ${prop => prop.theme.fontWeights.bold};
+  font-size: 12px;
+  font-weight: bold;
   line-height: 0.93;
   letter-spacing: 0.04em;
   text-decoration: none;
   text-transform: uppercase;
-  color: ${prop => prop.theme.colors.secondary};
+  color: #ecdfdf;
 
   :first-child {
     margin-right: ${prop => prop.theme.space[4]}px;
   }
 
   :hover {
-    color: ${prop => prop.theme.colors.accent};
+    color: pink;
   }
 
   &.active {
-    color: ${prop => prop.theme.colors.primary};
+    color: blue;
+  }
+`;
+export const LangSwitcher = styled.div`
+  display: flex;
+  align-items: center;
+  height: 2rem;
+  min-width: 42px;
+  position: relative;
+  z-index: 3;
+  align-self: center;
+  background-color: #ecdfdf;
+  border-radius: 5px;
+`;
+export const LangList = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-radius: 8px;
+  cursor: pointer;
+  min-width: 36px;
+  max-height: 30px;
+  padding-top: 5px;
+  position: absolute;
+  top: 0;
+  transition: all 0.3s ease;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    min-height: 26px;
+    height: 0%;
+    box-shadow: 0 4px 12px 0 transparent;
+    background-color: #ecdfdf;
+    transition: all 0.3s ease;
+    transition-delay: 0.1s;
+    border-radius: 6px;
+    background: 0 0;
+    box-shadow: none;
+  }
+
+  &.droped {
+    box-shadow: 0 4px 12px 0 rgb(0 0 0 / 10%);
+    background-color: grey;
+    max-height: unset;
+    margin-right: 3px;
+    &::before {
+      height: 100%;
+      box-shadow: 0 4px 12px 0 rgb(0 0 0 / 10%);
+      background-color: #ecdfdf;
+    }
+    button {
+      line-height: 1;
+      margin: 4px 0;
+      display: flex;
+      opacity: 1;
+      cursor: pointer;
+
+      &.active {
+        &::after {
+          transform: translateY(-50%) scaleY(-1.1) rotate(45deg);
+        }
+      }
+      &:not(.active) {
+        width: 100%;
+        opacity: 1;
+        pointer-events: all;
+      }
+    }
   }
 `;
 
 export const LangButton = styled.button`
-  cursor: pointer;
-  width: 20px;
-  height: 20px;
   border: none;
-  margin-right: 5px;
-  background-color: transparent;
-`;
+  background: #ecdfdf;
+  color: orange;
+  font-family: 'Gotham Pro';
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 1;
+  letter-spacing: 0.04em;
+  text-transform: uppercase;
+  color: black;
 
-export const ButtonContainer = styled.div`
-  display: flex;
-  align-items: center;
-  align-self: baseline;
-  padding-top: 8px;
-`;
-
-export const Lang = styled.ul`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 40px;
   position: relative;
-  left: 0;
-  display: none;
-  margin: 0;
-  padding: 0;
-  list-style: none;
-`;
+  transition: all 0.3s ease;
+  width: 100%;
+  z-index: 5;
+  cursor: pointer;
 
-export const LangItem = styled.option``;
+  display: flex;
+  gap: 4px;
+
+  &:hover {
+    color: orange;
+  }
+
+  &.active {
+    order: -1;
+    pointer-events: all;
+
+    &::after {
+      opacity: 1;
+      transform: translateY(-70%) scaleY(1.1) rotate(45deg);
+    }
+  }
+  &:not(.active) {
+    width: 0%;
+    pointer-events: none;
+    opacity: 0;
+  }
+`;
