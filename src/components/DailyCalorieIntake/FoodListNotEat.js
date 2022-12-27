2d4718cfaa8850ab34fce2a7264c9eee70e2fa16
ikarkam76 <ikarkam76@gmail.com>
2022-12-27 23:20:49 +0200

    finished pagination!

--- src/components/DailyCalorieIntake/FoodListNotEat.js
+++ src/components/DailyCalorieIntake/FoodListNotEat.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 import { number, string } from 'prop-types';
@@ -6,6 +6,7 @@ import Box from '@mui/material/Box';
 import ListItemButton from '@mui/material/ListItemButton';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown';
 import { ListText, ProductListText } from './FoodListNotEat.styled';
 import { selectUserParams } from 'redux/services/selectors';
@@ -16,57 +17,34 @@ export const CustomizedList = ({ number, category }) => {
   const [products, setProducts] = useState([]);
   const [anchorEl, setAnchorEl] = useState(null);
   const [open, setOpen] = useState(false);
-  const [page, setPage] = useState(2);
-  const [fetching, setFetching] = useState(false);
+  const [page, setPage] = useState(1);
   const isOpen = Boolean(anchorEl);
 
-  useEffect(() => {
-    if (open) {
-      document.addEventListener('scroll', scrollHandler);
-      return function () {
-        document.removeEventListener('scroll', scrollHandler);
-      };
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [open]);
-
-  useEffect(() => {
-    if (fetching) {
-      axios
-        .post(
-          `/api/products?category=${category}&currentPage=${page}&pageSize=10`,
-          user
-        )
-        .then(res => {
-          setProducts([...products, ...res.data]);
-          setPage(prevstate => prevstate + 1);
-        })
-        .finally(() => setFetching(false));
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [fetching]);
-
-  const scrollHandler = e => {
-    if (
-      e.target.documentElement.scrollHeight -
-        (e.target.documentElement.scrollTop + window.innerHeight) <
-      100
-    ) {
-      setFetching(true);
-    }
+  const getTitles = () => {
+    axios
+      .post(
+        `/api/products?category=${category}&currentPage=${page}&pageSize=25`,
+        user
+      )
+      .then(res => {
+        const titles = res.data.products.flatMap(prod => prod.title.ua);
+        setProducts(prev => [...prev, ...titles]);
+      });
+    setPage(prev => prev + 1);
   };
 
   const handleClick = event => {
     setAnchorEl(event.currentTarget);
     axios
       .post(
-        `/api/products?category=${category}&currentPage=1&pageSize=15`,
+        `/api/products?category=${category}&currentPage=${page}&pageSize=25`,
         user
       )
       .then(res => {
-        setProducts(res.data.products);
+        const titles = res.data.products.flatMap(prod => prod.title.ua)
+        setProducts(prev => [...prev, ...titles]);
       });
+    setPage(prev=>prev+1)
     setOpen(!open);
   };
 
@@ -113,17 +91,34 @@ export const CustomizedList = ({ number, category }) => {
           'aria-labelledby': 'basic-button',
         }}
       >
-        {open && !products[1] ? (
-          <Box sx={{ height: '400px', width: '300px' }}>
-            <Loader />
-          </Box>
-        ) : (
-          products.map(item => (
-            <MenuItem onClick={handleClose} key={item.title.ua}>
-              <ProductListText primary={'-  ' + item.title.ua} />
-            </MenuItem>
-          ))
-        )}
+        {open && (<div
+                id="scrollableDiv"
+                style={{
+                  height: 700,
+                  overflow: 'auto',
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}
+              >
+                <InfiniteScroll
+                  dataLength={products.length}
+                  next={getTitles}
+                  style={{ display: 'flex', flexDirection: 'column' }} 
+                  inverse={false} //
+                  hasMore={true}
+                  loader={<Box sx={{ height: '400px', width: '300px' }}>
+                            <Loader />
+                          </Box>}
+                  scrollableTarget="scrollableDiv"
+                >
+                  {products.map((item, index) => (
+                      <MenuItem onClick={handleClose} key={index}>
+                        <ProductListText primary={'-  ' + item} />
+                      </MenuItem>
+                    ))}
+                </InfiniteScroll>
+              </div>)
+          }
       </Menu>
     </Box>
   );
