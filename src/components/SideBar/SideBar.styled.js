5f24db2e352717100bfc58d5e8992123d197475c
ArtemStepanov1 <sparta.start@gmail.com>
2022-12-27 13:15:42 +0100

    fix style of sidebar

--- src/components/SideBar/SideBar.styled.js
+++ src/components/SideBar/SideBar.styled.js
@@ -3,37 +3,54 @@ import { ListItem } from '@mui/material';
 import backgroundTablet from '../../images/background/sidebar-bg-tablet.png';
 import backgroundDesktop from '../../images/background/sidebar-bg-desktop.png';
 
+
+
 export const BadFoodContainer = styled.div`
-  margin-top: 40px;
-  @media screen and (min-width: 768px) {
-    margin-left: 80px;
-    margin-top: 0px;
-  }
-  @media screen and (min-width: 1280px) {
-    margin-top: 60px;
-    margin-left: 0;
-  }
+margin-top: 40px;
+@media screen and (min-width: 768px) {
+  margin-left: 80px;
+  margin-top: 0px;
+}
+@media screen and (min-width: 1280px) {
+  margin-top: 60px;
+  margin-left: 0;
+}
 `;
 
 export const BadFoodPlaceholder = styled.div`
-  margin-top: 20px;
+margin-top: 20px;
 `;
 
 export const SummaryTitle = styled.h3`
-  margin-bottom: 20px;
-  font-family: Verdana;
-  font-weight: 700;
-  font-size: 14px;
-  line-height: 1.2;
-  letter-spacing: 0.04em;
-  color: #212121;
+margin-bottom: 20px;
+font-family: Verdana;
+font-weight: 700;
+font-size: 14px;
+line-height: 1.2;
+letter-spacing: 0.04em;
+color: #212121;
+`;
+
+export const BgWrapper = styled.div`
+  background: #f0f1f3;
+  @media (max-width: 767px) {
+    width: 100%;
+}
+@media (min-width: 768px) {
+  padding-right: auto;
+  padding-left: auto;
+}
+@media screen and (min-width: 1280px) {
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  width: 40%;
+}
 `;
 
 export const SideBarContainer = styled.div`
-  /* position: relative; */
   height: 100%;
-  display: flex;
-  flex-direction: column;
   padding: 40px 20px 52px 20px;
   font-family: Verdana;
   font-weight: 400;
@@ -43,20 +60,18 @@ export const SideBarContainer = styled.div`
   color: #9b9faa;
   background: #f0f1f3;
 
-  @media screen and (min-width: 768px) {
-    display: flex;
+  @media (max-width: 767px) {
+    max-width: 360px;
+    margin: 0 auto;
     flex-direction: row;
     align-items: baseline;
-    width: 768px;
-    padding: 80px 32px;
   }
   @media (min-width: 768px) {
+    display: flex;    
+    padding: 80px 32px;
     background-image: url(${backgroundTablet});
     background-repeat: no-repeat;
     background-position: bottom 0 right 0;
-    width: 768px;
-    margin-right: auto;
-    margin-left: auto;
   }
   @media screen and (min-width: 1280px) {
     background-image: url(${backgroundDesktop});
@@ -65,11 +80,6 @@ export const SideBarContainer = styled.div`
     display: grid;
     align-content: start;
     justify-content: center;
-    position: absolute;
-    top: 0;
-    right: 0;
-    height: 100%;
-    width: 40%;
     padding: 292px 94px 0 0;
   }
 `;
